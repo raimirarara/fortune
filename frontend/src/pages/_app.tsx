242a4f804d75c45158d7cc81cce609cf42760ba6
Rai <raimi.rarara@gmail.com>
2023-02-14 12:20:51 +0900

    mantine provider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,24 +1,26 @@
 import { AppFooter } from "@/components/AppFooter";
 import "@/styles/globals.css";
-import { AppShell } from "@mantine/core";
+import { AppShell, MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppShell
-      styles={{
-        main: {
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          alignItems: "center",
-          minHeight: "100dvh",
-          padding: "6rem",
-        },
-      }}
-      footer={<AppFooter />}
-    >
-      <Component {...pageProps} />
-    </AppShell>
+    <MantineProvider>
+      <AppShell
+        styles={{
+          main: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
+            minHeight: "100dvh",
+            padding: "6rem",
+          },
+        }}
+        footer={<AppFooter />}
+      >
+        <Component {...pageProps} />
+      </AppShell>
+    </MantineProvider>
   );
 }
